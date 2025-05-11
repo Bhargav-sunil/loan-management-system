eac146bd84a3a4353b4746be558f784eec5e0183
Bhargav-sunil <bhargavsunil2166@gmail.com>
2025-05-11 13:38:15 +0530

    Update UserDashboard.jsx

--- Frontend/src/pages/UserDashboard.jsx
+++ Frontend/src/pages/UserDashboard.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { useAuth } from '../context/AuthContext';
-import './UserDashboard.css';
+import './userDashboard.css';
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 
